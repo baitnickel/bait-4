26afdc3127a01d3774d16816463f26e2ae0745d6
Dan Dickinson <dan_dickinson@yahoo.com>
2023-03-15 14:10:25 -0700

    add video, SVG

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+import * as SVG from './lib/svg.js';
 const smugmug = 'https://photos.smugmug.com/Bait-4';
 const photoID = 'i-5FZ7TvW/0/284bbe68';
 const photoSize = 'S';
@@ -10,10 +11,10 @@ if (body) {
     let imageElement = new Image();
     // imageElement.src = 'https://photos.smugmug.com/Bait-4/i-5FZ7TvW/0/284bbe68/S/16-88-S.jpg';
     imageElement.src = `${smugmug}/${photoID}/${photoSize}/${photoFile}-${photoSize}.jpg`;
-    if (imageElement.width > 250)
-        imageElement.width = 250;
+    // if (imageElement.width > 250) imageElement.width = 250;
     body.append(imageElement);
-    // SVG.appendSVG(body, 'data/jmap7.svg', ['93', '95', '97', '99', '103', '104', 'J105']);
+    SVG.appendSVG(body, 'data/jmap7.svg', ['93', '95', '97', '99', '103', '104', 'J105']);
+    appendLines(body, '<iframe width="560" height="315" src="https://www.youtube.com/embed/INlBnm_1-sg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
 }
 function appendLines(body, lines) {
     let paragraph = document.createElement('p');
@@ -22,4 +23,3 @@ function appendLines(body, lines) {
     paragraph.innerHTML = lines;
     body.append(paragraph);
 }
-export {};
--- ts/index.ts
+++ ts/index.ts
@@ -18,11 +18,12 @@ if (body) {
 	let imageElement = new Image();
 	// imageElement.src = 'https://photos.smugmug.com/Bait-4/i-5FZ7TvW/0/284bbe68/S/16-88-S.jpg';
 	imageElement.src = `${smugmug}/${photoID}/${photoSize}/${photoFile}-${photoSize}.jpg`;
-	if (imageElement.width > 250) imageElement.width = 250;
+	// if (imageElement.width > 250) imageElement.width = 250;
 	body.append(imageElement);
 
-	// SVG.appendSVG(body, 'data/jmap7.svg', ['93', '95', '97', '99', '103', '104', 'J105']);
+	SVG.appendSVG(body, 'data/jmap7.svg', ['93', '95', '97', '99', '103', '104', 'J105']);
 
+	appendLines(body,'<iframe width="560" height="315" src="https://www.youtube.com/embed/INlBnm_1-sg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
 }
 
 function appendLines(body: HTMLElement, lines: string|string[]) {

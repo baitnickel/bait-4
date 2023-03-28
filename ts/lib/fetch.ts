1a6e6d3d4d88511fd629bba656e452c4329e4585
Dan Dickinson <dan_dickinson@yahoo.com>
2023-03-28 11:40:50 -0700

    modify fetch options

--- ts/lib/fetch.ts
+++ ts/lib/fetch.ts
@@ -1,7 +1,7 @@
 export async function fetchData(path: string, type: string = 'text') {
 	try {
 		const uri = new Request(path);
-		const response = await fetch(uri);
+		const response = await fetch(uri, {mode: 'same-origin'});
 		var data: any;
 		if (type == 'json' || path.endsWith('.json')) data = await response.json();
 		else if (type == 'blob') data = await response.blob();

0b043dd160e7227c6af13d017502ef096c77a987
Dan Dickinson <dan_dickinson@yahoo.com>
2025-08-25 15:53:05 -0700

    threads, CSS - revise module for new Widgets

--- js/threads.js
+++ js/threads.js
@@ -1,30 +1,29 @@
 import { Page } from './lib/page.js';
 import { Markup } from './lib/markup.js';
 import * as Fetch from './lib/fetch.js';
-import * as W from './lib/widgets-1.js';
-const Confirm = 'bait:confirm';
-const ConfirmEvent = new Event(Confirm);
-const Cancel = 'bait:cancel';
-const CancelEvent = new Event(Cancel);
+import * as W from './lib/widgets.js';
 const PAGE = new Page();
 if (!PAGE.backendAvailable) {
     window.alert(`Cannot connect to: ${PAGE.backend}`);
     window.history.back();
 }
 const QueryElement = document.createElement('div');
+QueryElement.className = 'threads-query-element';
 const OutputElement = document.createElement('div');
+OutputElement.className = 'threads-output-element';
+let ReverseSort;
 export function render() {
     PAGE.content.append(QueryElement);
     PAGE.content.append(OutputElement);
-    const modal = createModalDialog();
-    addQueryButton(modal);
+    const dialog = createModalDialog();
+    addQueryButton(dialog);
 }
-function addQueryButton(modal) {
+function addQueryButton(dialog) {
     const queryButton = document.createElement('button');
     queryButton.innerText = 'Enter Query';
     QueryElement.append(queryButton);
     queryButton.addEventListener('click', (e) => {
-        modal.open();
+        dialog.element.showModal();
     });
 }
 function getPassages(queryString) {
@@ -60,7 +59,7 @@ function displayPassages(passages, division) {
         let priorTag = '';
         for (const passage of passages) {
             if (passage.tag != priorTag)
-                lines.push(`# ${passage.tag}`);
+                lines.push(`#$ ${passage.tag}`);
             const fileName = (passage.file === null) ? '(none)' : `${passage.file.name}(${passage.section})`;
             lines.push(`## File: ${fileName}:`);
             lines.push(passage.text);
@@ -77,19 +76,19 @@ function sortPassages(passages) {
         let result = 0;
         result = a.tag.localeCompare(b.tag);
         if (!result && a.file !== null && b.file !== null) {
-            result = a.file.path.localeCompare(b.file.path);
+            // result = a.file.path.localeCompare(b.file.path);
+            /** Date types aren't recognized?? Using workaround... */
             // const aModified = a.file.modified as Date;
             // const bModified = b.file.modified as Date;
             // const aTime = aModified.getTime();
             // const bTime = bModified.getTime();
-            // result = aTime - bTime;
-            // console.log('a:', a.file.modified.getTime());
-            // console.log('b:', b.file.modified.getTime());
-            // if (a.file.modified !== undefined && b.file.modified !== undefined) {
-            // 	const aTime = a.file.modified.getTime();
-            // 	const bTime = b.file.modified.getTime();
-            // 	result = aTime - bTime;
-            // }
+            // result = (ReverseSort) ? bTime - aTime : aTime - bTime;
+            const aModified = a.file.modified.toString();
+            const bModified = b.file.modified.toString();
+            if (ReverseSort)
+                result = bModified.localeCompare(aModified);
+            else
+                result = aModified.localeCompare(bModified);
         }
         if (!result)
             result = a.section - b.section;
@@ -97,30 +96,19 @@ function sortPassages(passages) {
     });
 }
 function createModalDialog(rootPath = '', tags = '', tagPrefix = '') {
+    const dialog = new W.Dialog('Query Options');
     const rootValues = ['.', 'Content', 'Content/chapters', 'Content/drafts', 'Content/technical', 'Content/test-docs', 'notebook'];
-    const rootDropDown = new W.Select('Root Path: ');
-    rootDropDown.addOptions(rootValues, '--select--');
-    const tagPrefixText = new W.Text('Optional Tag Prefix: ', tagPrefix);
-    const tagsText = new W.Text('Space-Separated Tags: ', tags);
-    const cancelButton = new W.Button('Cancel', CancelEvent);
-    const confirmButton = new W.Button('Confirm', ConfirmEvent);
-    const modal = new W.Dialog('Query Options');
-    modal.element.className = 'threads-dialog';
-    modal.addWidget(rootDropDown);
-    modal.addWidget(tagPrefixText);
-    modal.addWidget(tagsText);
-    modal.addWidgets([cancelButton, confirmButton]);
-    modal.finish(document.body);
-    document.addEventListener(Cancel, () => {
-        modal.close();
-    });
-    document.addEventListener(Confirm, () => {
-        modal.close();
+    const rootDropDown = dialog.addSelect('Root Path:', rootValues);
+    const tagPrefixText = dialog.addText('Optional Tag Prefix:', '');
+    const tagsText = dialog.addText('Space-Separated Tags(+):', '');
+    const reverseSort = dialog.addCheckbox('Show Most Recent First:', true);
+    dialog.confirmButton.addEventListener('click', () => {
         let queryString = rootDropDown.value;
         if (tagPrefixText.value)
             queryString += ` ${tagPrefixText.value}-`;
         queryString += ` ${tagsText.value}`;
+        ReverseSort = reverseSort.checked;
         getPassages(queryString);
     });
-    return modal;
+    return dialog;
 }
--- ts/threads.ts
+++ ts/threads.ts
@@ -1,13 +1,8 @@
 import { Page } from './lib/page.js';
 import { Markup } from './lib/markup.js';
 import * as Fetch from './lib/fetch.js';
-import * as W from './lib/widgets-1.js';
+import * as W from './lib/widgets.js';
 import * as T from './lib/types.js';
-
-const Confirm = 'bait:confirm';
-const ConfirmEvent = new Event(Confirm);
-const Cancel = 'bait:cancel';
-const CancelEvent = new Event(Cancel);
 
 const PAGE = new Page();
 if (!PAGE.backendAvailable) {
@@ -16,22 +11,24 @@ if (!PAGE.backendAvailable) {
 }
 
 const QueryElement = document.createElement('div');
+QueryElement.className = 'threads-query-element';
 const OutputElement = document.createElement('div');
+OutputElement.className = 'threads-output-element';
+let ReverseSort: boolean;
 
 export function render() {
 	PAGE.content.append(QueryElement);
 	PAGE.content.append(OutputElement);
-	const modal = createModalDialog();
-	addQueryButton(modal);
+	const dialog = createModalDialog();
+	addQueryButton(dialog);
 }
 
-function addQueryButton(modal: W.Dialog) {
+function addQueryButton(dialog: W.Dialog) {
 	const queryButton = document.createElement('button');
 	queryButton.innerText = 'Enter Query';
 	QueryElement.append(queryButton);
-
 	queryButton.addEventListener('click', (e) => {
-		modal.open();
+		dialog.element.showModal();
 	});
 }
 
@@ -68,7 +65,7 @@ function displayPassages(passages: T.ThreadPassage[]|null, division: HTMLDivElem
 		sortPassages(passages);
 		let priorTag = '';
 		for (const passage of passages) {
-			if (passage.tag != priorTag) lines.push(`# ${passage.tag}`);
+			if (passage.tag != priorTag) lines.push(`#$ ${passage.tag}`);
 			const fileName = (passage.file === null) ? '(none)' : `${passage.file.name}(${passage.section})`;
 			lines.push(`## File: ${fileName}:`);
 			lines.push(passage.text);
@@ -86,21 +83,18 @@ function sortPassages(passages: T.ThreadPassage[]) {
 		let result = 0;
 		result = a.tag.localeCompare(b.tag);
 		if (!result && a.file !== null && b.file !== null) {
+			// result = a.file.path.localeCompare(b.file.path);
 
-			result = a.file.path.localeCompare(b.file.path);
+			/** Date types aren't recognized?? Using workaround... */
 			// const aModified = a.file.modified as Date;
 			// const bModified = b.file.modified as Date;
 			// const aTime = aModified.getTime();
 			// const bTime = bModified.getTime();
-			// result = aTime - bTime;
-
-			// console.log('a:', a.file.modified.getTime());
-			// console.log('b:', b.file.modified.getTime());
-			// if (a.file.modified !== undefined && b.file.modified !== undefined) {
-			// 	const aTime = a.file.modified.getTime();
-			// 	const bTime = b.file.modified.getTime();
-			// 	result = aTime - bTime;
-			// }
+			// result = (ReverseSort) ? bTime - aTime : aTime - bTime;
+			const aModified = a.file.modified.toString();
+			const bModified = b.file.modified.toString();
+			if (ReverseSort) result = bModified.localeCompare(aModified);
+			else result = aModified.localeCompare(bModified);
 		}
 		if (!result) result = a.section - b.section;
 		return result;
@@ -108,32 +102,20 @@ function sortPassages(passages: T.ThreadPassage[]) {
 }
 
 function createModalDialog(rootPath = '', tags = '', tagPrefix = '') {
+	const dialog = new W.Dialog('Query Options')
 	const rootValues = ['.', 'Content','Content/chapters','Content/drafts','Content/technical','Content/test-docs', 'notebook'];
-	const rootDropDown = new W.Select('Root Path: ');
-	rootDropDown.addOptions(rootValues, '--select--');
-	const tagPrefixText = new W.Text('Optional Tag Prefix: ', tagPrefix);
-	const tagsText = new W.Text('Space-Separated Tags: ', tags);
-	const cancelButton = new W.Button('Cancel', CancelEvent);
-	const confirmButton = new W.Button('Confirm', ConfirmEvent);
+	const rootDropDown = dialog.addSelect('Root Path:', rootValues);
+	const tagPrefixText = dialog.addText('Optional Tag Prefix:', '');
+	const tagsText = dialog.addText('Space-Separated Tags(+):', '');
+	const reverseSort = dialog.addCheckbox('Show Most Recent First:', true);
 
-	const modal = new W.Dialog('Query Options')
-	modal.element.className = 'threads-dialog';
-	modal.addWidget(rootDropDown);
-	modal.addWidget(tagPrefixText);
-	modal.addWidget(tagsText);
-	modal.addWidgets([cancelButton, confirmButton]);
-	modal.finish(document.body);
-
-	document.addEventListener(Cancel, () => {
-		modal.close();
-	});
-	document.addEventListener(Confirm, () => {
-		modal.close();
+	dialog.confirmButton.addEventListener('click', () => {
 		let queryString = rootDropDown.value;
 		if (tagPrefixText.value) queryString += ` ${tagPrefixText.value}-`;
 		queryString += ` ${tagsText.value}`;
+		ReverseSort = reverseSort.checked;
 		getPassages(queryString); 
 	});
 
-	return modal;
+	return dialog;
 }

b66040c8b3ea87a57655f7ba37883ae7ba369584
Dan Dickinson <dan_dickinson@yahoo.com>
2025-08-29 16:46:38 -0700

    WIP timeline - working version w/ most features

--- js/lib/page.js
+++ js/lib/page.js
@@ -82,8 +82,8 @@ export class Page {
         if (this.local) { /** Add test pages */
             MenuItems.push({ module: 'articles', parameters: ['path=Content/drafts'], text: 'Drafts', icon: '' });
             MenuItems.push({ module: 'threads', parameters: [], text: 'Threads', icon: '' });
-            MenuItems.push({ module: 'carousel', parameters: [], text: 'Carousel', icon: '' });
             MenuItems.push({ module: 'timeline', parameters: [], text: 'Timeline', icon: '' });
+            MenuItems.push({ module: 'carousel', parameters: [], text: 'Carousel', icon: '' });
             // MenuItems.push({module: 'articles', parameters: ['path=Content/test-redwords'], text: 'Red Words', icon: ''});
             // MenuItems.push({module: 'test-cookies', parameters: [], text: 'Cookies', icon: ''});
             // MenuItems.push({module: 'test-lyrics', parameters: [], text: 'Lyrics', icon: ''});
--- js/timeline.js
+++ js/timeline.js
@@ -1,26 +1,105 @@
 import { Page } from './lib/page.js';
 import * as T from './lib/types.js';
 import * as Fetch from './lib/fetch.js';
-const PAGE = new Page();
+import * as W from './lib/widgets.js';
+const PAGE = new Page(true);
 if (!PAGE.backendAvailable) {
     window.alert(`Cannot connect to: ${PAGE.backend}`);
     window.history.back();
 }
 const TimedEvents = await Fetch.api(`${PAGE.backend}/timeline`);
+const Options = {
+    sortAscending: true,
+    fromYear: '',
+    thruYear: '',
+    birthdate: '1952/06/21',
+    keywords: '',
+};
+const QueryElement = document.createElement('div');
+QueryElement.className = 'timeline-query-element';
+const TimelineElement = document.createElement('div');
+TimelineElement.className = 'timeline-container';
 export function render() {
+    PAGE.content.append(QueryElement);
+    PAGE.content.append(TimelineElement);
     if (!TimedEvents)
         PAGE.appendParagraph(PAGE.content, 'The timeline API returned nothing!');
-    else
-        processTimedEvents(TimedEvents);
-}
-function processTimedEvents(timedEvents) {
-    const lines = [];
-    for (const timedEvent of timedEvents) {
-        const dateString = getDateString(timedEvent.dateValue, timedEvent.precision);
-        const line = `${dateString}: ${timedEvent.description}`;
-        lines.push(line);
+    else {
+        const dialog = createModalDialog(Options);
+        addQueryButton(dialog);
     }
-    PAGE.appendParagraph(PAGE.content, lines);
+}
+function addQueryButton(dialog) {
+    const queryButton = document.createElement('button');
+    queryButton.innerText = 'Timeline Query';
+    QueryElement.append(queryButton);
+    queryButton.addEventListener('click', (e) => {
+        dialog.element.showModal();
+    });
+}
+function processTimedEvents(options) {
+    let timedEvents = TimedEvents.slice(); /** make a copy of the original */
+    if (!(timedEvents && timedEvents.length))
+        PAGE.appendParagraph(PAGE.content, 'The timeline API returned nothing!');
+    else {
+        timedEvents = filterEvents(timedEvents, options);
+        timedEvents.sort((a, b) => {
+            let result = (options.sortAscending) ? a.dateValue - b.dateValue : b.dateValue - a.dateValue;
+            if (!result)
+                result = (options.sortAscending) ? a.precision - b.precision : b.precision - a.precision;
+            return result;
+        });
+        displayGrid(TimelineElement, timedEvents);
+    }
+}
+function filterEvents(timedEvents, options) {
+    if (options.fromYear) {
+        const fromDate = new Date(Number(options.fromYear), 0, 1);
+        timedEvents = timedEvents.filter((e) => e.dateValue >= fromDate.valueOf());
+    }
+    if (options.thruYear) {
+        const thruDate = new Date(Number(options.thruYear), 0, 1);
+        timedEvents = timedEvents.filter((e) => e.dateValue < thruDate.valueOf());
+    }
+    if (options.keywords) {
+        const keywords = options.keywords.trim().split(/\s+/);
+        timedEvents = timedEvents.filter((e) => {
+            let eWords = e.description.trim().split(/\s+/);
+            eWords = eWords.map(element => element.toLowerCase());
+            let found = false;
+            for (const keyword of keywords) {
+                if (eWords.includes(keyword.toLowerCase())) {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        });
+    }
+    return timedEvents;
+}
+function displayGrid(container, timedEvents, birthday = new Date(1952, 5, 21)) {
+    container.innerHTML = '';
+    for (const timedEvent of timedEvents) {
+        const date = new Date(timedEvent.dateValue);
+        const dateString = getDateString(date, timedEvent.precision);
+        const age = getAge(birthday, date);
+        const grade = getGrade(birthday, date);
+        const ageGrade = (grade) ? `${age} (${grade})` : `${age}`;
+        const fields = [dateString, timedEvent.description, ageGrade];
+        for (let i = 0; i < fields.length; i += 1) {
+            const item = document.createElement('div');
+            if (i == 0)
+                item.className = 'timeline-date';
+            else if (i == 1)
+                item.className = 'timeline-description';
+            else
+                item.className = 'timeline-age';
+            let value = fields[i];
+            item.innerHTML = value;
+            container.append(item);
+        }
+    }
 }
 function getDateString(date, precision) {
     if (typeof date == 'number')
@@ -36,4 +115,82 @@ function getDateString(date, precision) {
     if (format)
         dateString = T.DateString(date, format);
     return dateString;
+}
+/**
+ * Given a `birthDate` and a `targetDate` (such as the current date), return a
+ * string representing the age of the person whose birth date you've entered.
+ * Age is returned as a string--an integer if the target month and day is the
+ * same as the birth month and day, otherwise a number with a single, unrounded
+ * decimal place (.0 to .9).
+ */
+function getAge(birthDate, targetDate) {
+    let age = '';
+    const targetMonth = targetDate.getMonth();
+    const targetDay = targetDate.getDate();
+    const targetYear = targetDate.getFullYear();
+    const birthMonth = birthDate.getMonth();
+    const birthDay = birthDate.getDate();
+    const birthYear = birthDate.getFullYear();
+    const yearsDifference = targetYear - birthYear;
+    /** calculate days into year for both dates, pretending they are in the same leap year */
+    const dailyMilliseconds = 24 * 60 * 60 * 1000;
+    const startOfYear = Date.UTC(2000, 0, 0);
+    const targetDayNumber = (Date.UTC(2000, targetMonth, targetDay) - startOfYear) / dailyMilliseconds;
+    const birthDayNumber = (Date.UTC(2000, birthMonth, birthDay) - startOfYear) / dailyMilliseconds;
+    const daysDifferent = targetDayNumber - birthDayNumber;
+    if (daysDifferent == 0)
+        age = `${yearsDifference}`; /** exact birthday */
+    else if (daysDifferent < 0) { /** target is earlier than birthday */
+        const fraction = ((366 - Math.abs(daysDifferent)) / 366).toString().substring(1, 3);
+        age = `${yearsDifference - 1}${fraction}`;
+    }
+    else { /** target is later than birthday */
+        const fraction = (daysDifferent / 366).toString().substring(1, 3);
+        age = `${yearsDifference}${fraction}`;
+    }
+    return age;
+}
+/**
+ * Given a `birthDate`, return the school grade that a person born on this
+ * date would typically be attending at the time of `this` event. We make
+ * quite a few assumptions here--we assume the person started Kindergarten
+ * at age 5 and did not repeat or skip any grades all the way through four
+ * years of college. (To be more exact would require passing in a profile
+ * containing dates for each grade.)
+ */
+function getGrade(birthDate, targetDate) {
+    let grade = '';
+    const grades = ['K', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'FR', 'SO', 'JR', 'SR'];
+    /** using kindergarten cutoff date: Sep 1 */
+    const cutoffMonth = 8; /** month offset */
+    const cutoffDay = 1;
+    /** get the most recently past cutoff date */
+    let cutoff = new Date(targetDate.getFullYear(), cutoffMonth, cutoffDay);
+    if (cutoff.valueOf() > targetDate.valueOf()) {
+        /** this year's cutoff date is in the future; use last year's cutoff date */
+        cutoff = new Date(targetDate.getFullYear() - 1, cutoffMonth, cutoffDay);
+    }
+    const ageAtCutoff = Math.floor(Number(getAge(birthDate, cutoff)));
+    const delta = 5;
+    const ageDelta = ageAtCutoff - delta;
+    if (ageDelta >= 0 && ageDelta < grades.length)
+        grade = grades[ageDelta];
+    return grade;
+}
+function createModalDialog(options) {
+    const dialog = new W.Dialog('Timeline Options');
+    const fromYear = dialog.addText('From Year:', options.fromYear);
+    const thruYear = dialog.addText('Thru Year:', options.thruYear);
+    const keywords = dialog.addText('Search Keywords:', options.keywords);
+    const birthdate = dialog.addText('Birthdate:', options.birthdate);
+    const sortAscending = dialog.addCheckbox('Sort Ascending:', options.sortAscending);
+    dialog.confirmButton.addEventListener('click', () => {
+        options.sortAscending = sortAscending.checked;
+        options.fromYear = fromYear.value;
+        options.thruYear = thruYear.value;
+        options.birthdate = birthdate.value;
+        options.keywords = keywords.value;
+        processTimedEvents(options);
+    });
+    return dialog;
 }

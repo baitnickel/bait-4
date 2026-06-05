962c66acafaceb3ba61b127b1f5c51ba86523907
Dan Dickinson <dan_dickinson@yahoo.com>
2026-06-05 16:49:36 -0700

    media, audio - mostly working, issues when track is paused

--- ts/audio.ts
+++ ts/audio.ts
@@ -14,52 +14,55 @@ if (!PAGE.backendAvailable) {
 
 // if processing multiple playlists, we must merge them all into one
 const MediaFolders = '../media/audio';
-const PlaylistFolders = ['test'];
+const PlaylistFolders = ['test-piano', 'test-strings', 'test-harp', 'wake'];
 const Playlists: Media.Playlist[] = [];
+
 for (const playlistFolder of PlaylistFolders) {
 	const indexFile = `${MediaFolders}/${playlistFolder}/_index.json`;
 	const playlist = await Fetch.json<Media.Playlist>(indexFile);
 	Playlists.push(playlist);
 }
-
+const PlaylistTracks = Media.PlaylistTracks(Playlists);
+const NowPlaying = document.createElement('div');
 const ControlsBlock = document.createElement('div');
 ControlsBlock.id = 'controls-block';
 let MainAudioElement = document.createElement('audio');
 MainAudioElement.controls = true;
 ControlsBlock.append(MainAudioElement);
 
-let CurrentPlaylist = 0;
-let CurrentTrack = 0;
+let CurrentPlaylistTrack = 0;
 
 export function render() {
-	PAGE.addHeading('Audio Playlists', 2);
+	PAGE.addHeading('Playlist Console', 2);
+	PAGE.content.append(NowPlaying);
 	PAGE.content.append(ControlsBlock);
-	runPlaylist(CurrentPlaylist);
-	// Media.RunPlaylist(MediaFolders, Playlists[0], MainAudioElement);
-}
-
-function runPlaylist(index: number) {
-	Media.RunPlaylist(MediaFolders, Playlists[index], MainAudioElement);
+	Media.RunPlaylists(MediaFolders, PlaylistTracks, MainAudioElement);
 }
 
 document.addEventListener(Media.PlaylistLoaded, () => {
-	console.log(`playlist loaded: ${Playlists[CurrentPlaylist].folder}`);
+	console.log(`playlist loaded (${PlaylistTracks.length} tracks)`);
 });
 
+// The TrackPlaying event is dispatched when a track begins playing, but also
+// when a track is paused and restarted, or when its progress bar is moved. This
+// is a problem. Maybe TrackEnded is a better indication (but we'd have to treat
+// the first track in the playlist as an exception).
 document.addEventListener(Media.TrackPlaying, () => {
-	console.log(`track playing: ${Playlists[CurrentPlaylist].sequence[CurrentTrack]}`);
-	CurrentTrack += 1;
+	const folder = PlaylistTracks[CurrentPlaylistTrack].folder;
+	const file = PlaylistTracks[CurrentPlaylistTrack].file;
+	const playlistTitle = PlaylistTracks[CurrentPlaylistTrack].playlistTitle;
+	const trackTitle = PlaylistTracks[CurrentPlaylistTrack].trackTitle;
+	NowPlaying.innerHTML = `<h3>${playlistTitle}: ${trackTitle}</h3>`;
+	console.log(`track playing: [${CurrentPlaylistTrack}] ${folder}/${file}`)
+	CurrentPlaylistTrack += 1;
+});
+
+document.addEventListener(Media.PlaylistEnded, () => {
+	NowPlaying.innerHTML = '';
+	console.log(`playlist ended`);
 });
 
 // document.addEventListener(Media.TrackEnded, () => {
 // 	// console.log(`track ended: ${Playlists[CurrentPlaylist].sequence[CurrentTrack]}`);
 // 	CurrentTrack += 1;
 // });
-
-document.addEventListener(Media.PlaylistEnded, () => {
-	console.log(`playlist ended: ${Playlists[CurrentPlaylist].folder}`);
-	// won't work right - on each Playlist load, the user must click on the Play button
-	// CurrentPlaylist += 1;
-	CurrentTrack = 0;
-	// if (CurrentPlaylist < Playlists.length) runPlaylist(CurrentPlaylist);
-});
--- ts/lib/media.ts
+++ ts/lib/media.ts
@@ -26,6 +26,76 @@ export type Playlist = {
 	tracks: Track[],
 };
 
+/** a flattened object, merged Playlists */
+export type PlaylistTrack = {
+	folder: string,
+	file: string,                 /** file names cannot contain the hash (#) character! */
+	playlistTitle: string,
+	trackTitle: string
+};
+
+/**
+ * Given a Playlist object or an array of Playlist Objects, return an array of
+ * PlaylistTracks, flattened Tracks that can be combined and played in sequence.
+ */
+export function PlaylistTracks(playlists: Playlist[]) {
+	const playlistTracks: PlaylistTrack[] = [];
+	for (const playlist of playlists) {
+		for (const sequencedTrack of playlist.sequence) {
+			const track = playlist.tracks.find((element) => element.file == sequencedTrack);
+			// if (track) {
+			const playlistTrack = {
+				folder: playlist.folder,
+				file: sequencedTrack, // track.file,
+				playlistTitle: playlist.title,
+				trackTitle: (track) ? track.title : sequencedTrack,
+			}
+			playlistTracks.push(playlistTrack);
+			// }
+		}
+	}
+	return playlistTracks;
+};
+
+/**
+ * Given a `path` under which Playlist folders can be found, an array of
+ * `playlistTracks` (one or more Playlist objects), and an `audioElement`, load
+ * and play each of the audio files in sequence. When the optional `loop`
+ * parameter is set to 'true', the entire playlist will be repeated endlessly
+ * (or until the user refreshes their page).
+ */
+export function RunPlaylists(path: string, playlistTracks: PlaylistTrack[], audioElement: HTMLAudioElement, loop = false) {
+	const playlistLoaded = new Event(PlaylistLoaded);
+	const trackPlaying = new Event(TrackPlaying);
+	const playlistEnded = new Event(PlaylistEnded);
+
+	let tracks = {
+		index: 0,
+		next: function() { this.index = (this.index + 1) % playlistTracks.length },
+		select: function() {
+			const folder = playlistTracks[this.index].folder;
+			const file = playlistTracks[this.index].file;
+			return `${path}/${folder}/${file}`;
+		}
+	}
+	audioElement.src = tracks.select();
+	document.dispatchEvent(playlistLoaded);
+
+	audioElement.addEventListener('playing', () => {
+		document.dispatchEvent(trackPlaying);
+	})
+
+	audioElement.addEventListener('ended', (e: Event) => {
+		tracks.next();
+		audioElement.src = tracks.select();
+		if (tracks.index != 0 || loop) {
+			audioElement.load();
+			audioElement.play();
+		}
+		else document.dispatchEvent(playlistEnded);
+	});
+}
+
 /**
  * Given a `playlist` containing a sequenced list of audio files and an
  * HTMLAudioElement, load and play each of the audio files in succession. When

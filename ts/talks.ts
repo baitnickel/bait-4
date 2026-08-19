3f19b91154495791496759ead19d4e446ed3be8d
Dan Dickinson <dan_dickinson@yahoo.com>
2026-08-19 16:07:24 -0700

    talks - WIP

--- ts/talks.ts
+++ ts/talks.ts
@@ -2,15 +2,18 @@ import { Page } from './lib/page.js';
 import * as T from './lib/types.js';
 import * as Fetch from './lib/fetch.js';
 
-const PAGE = new Page(false, false);
+const PAGE = new Page();
 if (!PAGE.backendAvailable) {
 	window.alert(`Cannot connect to: ${PAGE.backend}`);
 	window.history.back();
 }
+console.log('v26.08.19.11.19');
 const AudioDataset = await Fetch.api<T.AudioDataset>(`${PAGE.backend}/media/talks`);
 
 export function render() {
 	PAGE.setTitle('Talks List');
+	const message = (AudioDataset === null) ? 'Nothing Loaded' : `Loaded ${AudioDataset.data.length} Records`;
+	PAGE.appendParagraph(PAGE.content, message);
 	// Dialog = createModalDialog();
 	// document.body.append(Dialog.element);
 	// Dialog.element.showModal();
